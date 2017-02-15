7b8659f144950609e1041606ff6650a1c11d5846
zaru <zarutofu@gmail.com>
2017-02-15 03:30:50 +0000

    Added sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/" => "pages#index"
+  get "sign_in" => "user/sessions#new"
+  delete "sign_out" => "user/sessions#destroy"
   namespace :user do
     resources :registrations, only: [:new, :create]
+    resources :sessions, only: :create
   end
 end
