18a41d90d12cf35cd44141a784a2a65997aa05d1
zaru@sakuraba <zarutofu@gmail.com>
2017-02-27 09:55:09 +0900

    Added article

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170220031036) do
+ActiveRecord::Schema.define(version: 20170227005110) do
+
+  create_table "articles", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "title"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
