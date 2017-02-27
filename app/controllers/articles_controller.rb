319e1d91a4b86ee432cb91219f03725b4370b608
zaru@sakuraba <zarutofu@gmail.com>
2017-02-27 10:01:44 +0900

    Fixed relation user_id for article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :require_login
 
   # GET /articles
   # GET /articles.json
@@ -25,6 +26,7 @@ class ArticlesController < ApplicationController
   # POST /articles.json
   def create
     @article = Article.new(article_params)
+    @article.user = current_user
 
     respond_to do |format|
       if @article.save
@@ -69,6 +71,6 @@ class ArticlesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def article_params
-      params.require(:article).permit(:user_id, :title, :description)
+      params.require(:article).permit(:title, :description)
     end
 end
